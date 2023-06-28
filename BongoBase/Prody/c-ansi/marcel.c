d0ba613d4b380198de4c9472658e4e9e74363d72
Puduku <pierre.bodin@skynet.be>
2023-06-28 20:25:23 +0200

    marcel's mime...

---
+++ BongoBase/Prody/c-ansi/marcel.c
@@ -0,0 +1,14 @@
+// c-ansi/marcel.c, version 1.93
+// (c) Atos-Euronext Belgium - 2003
+// (c) Puduku - 2023
+
+
+#include "c-ansi/marcel.h"
+
+const struct TOKEN_DEFINITION p_commonMimeTypes = {
+  { "application/octet-stream",  APPLICATION_OCTET_STREAM__COMMON_MIME_TYPE },
+  { "application/x-www-form-urlencoded", APPLICATION_X_WWW_FORM_URLENCODED__COMMON_MIME_TYPE },
+  { "text/html", TEXT_HTML__COMMON_MIME_TYPE },
+  { "text/plain" TEXT_PLAIN__COMMON_MIME_TYPE },
+} ;
+
---
+++ BongoBase/Prody/c-ansi/marcel.h
@@ -0,0 +1,35 @@
+// c-ansi/marcel.h, version 1.93
+// (c) Atos-Euronext Belgium - 2003
+// (c) Puduku - 2023
+
+// Description :
+// Necessary stuff to handle standard MIME types 
+
+#ifndef __C_ANSI_MARCEL_H_INCLUDED__
+#define __C_ANSI_MARCEL_H_INCLUDED__
+
+#include "c-ansi/g-token.h"
+
+
+// Marcel's common MIME types enumeration
+enum {
+  APPLICATION_OCTET_STREAM__COMMON_MIME_TYPE = 0,
+  APPLICATION_X_WWW_FORM_URLENCODED__COMMON_MIME_TYPE,
+  TEXT_HTML__COMMON_MIME_TYPE,
+  TEXT_PLAIN__COMMON_MIME_TYPE,
+  COMMON_MIME_TYPES_NUMBER 
+} ; 
+
+// TODO: add indexes
+
+extern const struct TOKEN_DEFINITION p_commonMimeTypes ;
+
+static inline int m_MarcelCreateInstance(G_STRINGS_HANDLE *azh_handle) {
+  m_TRACK_IF(G_STRINGS_CREATE_INSTANCE( azh_handle,COMMON_MIME_TYPES_NUMBER) != RETURNED)
+  m_G_STRINGS_IMPORT_TOKENS(*azh_handle,p_commonMimeTypes,  COMMON_MIME_TYPES_NUMBER)
+  m_TRACK_IF(GStringsFreeze(*azh_handle,NULL) != RETURNED)
+  return RETURNED;
+} // m_MarcelCreateInstance
+
+
+#endif // __C_ANSI_MARCEL_H_INCLUDED__
