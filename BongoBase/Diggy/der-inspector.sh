b5303b410029cc61858c9a0108cc91b7f2b051ee
Puduku <130610270+Puduku@users.noreply.github.com>
2024-05-22 10:48:14 +0200

    Update der-inspector.sh

--- BongoBase/Diggy/der-inspector.sh
+++ BongoBase/Diggy/der-inspector.sh
@@ -2,9 +2,9 @@
 # Diggy/der-inspector.sh
 . mama.esh
 
-##############################################################################################
-plot="Collect all the coconuts in Diggy's garden (i.e. highlight doc in Diggy's header files)"
-##############################################################################################
+########################################
+plot="Highlight Diggy API documentation"
+########################################
 
 . der-inspector.esh
 
