60fd6c5857e5fc39a398e2462d4e54b422dfd75b
Puduku <pierre.bodin@skynet.be>
2025-10-29 20:08:41 +0100

    Update engels.c

--- BongoBase/Prody/Marx/c-ansi/engels.c
+++ BongoBase/Prody/Marx/c-ansi/engels.c
@@ -33,57 +33,25 @@ enum {
   DATE__REFINED_MATTER,
 } ;
 
-// TRACT_OR_PIVOT_MODEL:
-
-#define b_PIVOT b_TRUE
-#define b_TRACT !b_PIVOT 
-
-struct TRACT_OR_PIVOT_MODEL {
-  m_DECLARE_MAGIC_FIELD(TRACT_OR_PIVOT_MODEL_HANDLE)
-  char b_pivot ;
-  union {
-    int c_pivotModelEntry ; // Only significant with "pivot"
-    int c_tractModelEntry; // Only significant if "not" pivot (aka aggregate)...
-  } select;
-} ;
-typedef struct TRACT_OR_PIVOT_MODEL *TRACT_OR_PIVOT_MODEL_HANDLE;
-
-//
-static int TractOrPivotModelCreateInstance(TRACT_OR_PIVOT_MODEL_HANDLE *azh_handle,char b_pivot,
-  int c_pivotModelEntry, int c_tractModelEntry) {
-  m_DIGGY_BOLLARD_S()
-  m_MALLOC_INSTANCE(*azh_handle)
-  m_ASSIGN_MAGIC_FIELD(TRACT_OR_PIVOT_MODEL_HANDLE,*azh_handle)
-  (*azh_handle)->b_pivot = b_pivot; 
-  (*azh_handle)->select.c_pivotModelEntry = c_pivotModelEntry; 
-  (*azh_handle)->select.c_tractModelEntry = c_tractModelEntry;
-  m_DIGGY_RETURN(RETURNED)
-} // TractModelCreateInstance
-
-// NAMED_OBJECT_DESTROY_INSTANCE_FUNCTION
-static int TractOrPivotModelDestroyInstance(void *xhr_handle) {
-  m_DIGGY_BOLLARD_S()
-  TRACT_OR_PIVOT_MODEL_HANDLE xh_handle = (TRACT_OR_PIVOT_MODEL_HANDLE) xhr_handle;
-  m_CHECK_MAGIC_FIELD(TRACT_OR_PIVOT_MODEL_HANDLE,xh_handle)
-  free(xh_handle);
-  m_DIGGY_RETURN(RETURNED)
-} // TractOrPivotModelDestroyInstance
- 
 // PIVOT_MODEL:
 
 struct PIVOT_MODEL {
   m_DECLARE_MAGIC_FIELD(PIVOT_MODEL_HANDLE)
-  int n_rawSize;
+  struct P_STRING v_name; // that of as "named object"
+  int n_minSize;
+  int n_maxSize;
   int rawMatterFlags;
   int refinedMatter; // must be compatible with raw matter flags
 } ;
 typedef struct PIVOT_MODEL *PIVOT_MODEL_HANDLE;
 
 //
-static int PivotModelCreateInstance(PIVOT_MODEL_HANDLE *azh_handle) {
+static int PivotModelCreateInstance(PIVOT_MODEL_HANDLE *azh_handle, struct P_STRING name) {
   m_DIGGY_BOLLARD_S()
   m_MALLOC_INSTANCE(*azh_handle)
-  m_ASSIGN_MAGIC_FIELD(PIVOT_MODEL_HANDLE,*azh_handle)
+  PIVOT_MODEL_HANDLE handle = *azh_handle;
+  m_ASSIGN_MAGIC_FIELD(PIVOT_MODEL_HANDLE,handle)
+  handle->v_name = name;
   m_DIGGY_RETURN(RETURNED)
 } // PivotModelCreateInstance
 
@@ -99,22 +67,57 @@ static int PivotModelDestroyInstance (void *xhr_handle) {
   m_DIGGY_RETURN(RETURNED)
 } // PivotModelDestroyInstance
  
+// TRACT_OR_PIVOT_MODEL:
+
+#define b_PIVOT b_TRUE
+#define b_TRACT !b_PIVOT 
+
+// ULTRA green type:
+struct TRACT_OR_PIVOT_MODEL {
+  m_DECLARE_MAGIC_FIELD(TRACT_OR_PIVOT_MODEL_HANDLE)
+  int minOccurencesNumber;
+  int maxOccurencesNumber;
+  char b_pivot ;
+  union {
+    int c_pivotModelEntry ; // Only significant with "pivot"
+    int c_tractModelEntry; // Only significant if "not" pivot (aka aggregate)...
+  } select;
+} ;
+typedef struct TRACT_OR_PIVOT_MODEL *TRACT_OR_PIVOT_MODEL_STUFF;
+
+//
+static int TractOrPivotModelInitialize(TRACT_OR_PIVOT_MODEL_STUFF stuff, int minOccurencesNumber,
+  int maxOccurencesNumber, char b_pivot, int c_pivotModelEntry, int c_tractModelEntry) {
+  m_DIGGY_BOLLARD_S()
+  stuff->minOccurencesNumber = minOccurencesNumber;
+  stuff->maxOccurencesNumber = maxOccurencesNumber;
+  stuff->b_pivot = b_pivot; 
+  stuff->select.c_pivotModelEntry = c_pivotModelEntry; 
+  stuff->select.c_tractModelEntry = c_tractModelEntry;
+  m_DIGGY_RETURN(RETURNED)
+} // TractOrPivotModelInitialize
+
 
 // TRACT_MODEL:
 
 struct TRACT_MODEL {
   m_DECLARE_MAGIC_FIELD(TRACT_MODEL_HANDLE)
-  G_STRINGS_HANDLE h_tractOrPivotModelsHandle ; // managed as NAMED OBJECTs 
+  struct P_STRING v_name; // that of as "named object"
+  GREEN_COLLECTION_HANDLE h_tractOrPivotModelsHandle;
 } ;
 typedef struct TRACT_MODEL *TRACT_MODEL_HANDLE;
 
 //
-static int TractModelCreateInstance(TRACT_MODEL_HANDLE *azh_handle) {
+static int TractModelCreateInstance(TRACT_MODEL_HANDLE *azh_handle,
+  int expectedTractOrPivotModelsNumber) {
   m_DIGGY_BOLLARD_S()
   m_MALLOC_INSTANCE(*azh_handle)
-  m_ASSIGN_MAGIC_FIELD(TRACT_MODEL_HANDLE,*azh_handle)
-  m_TRACK_IF(NAMED_OBJECTS_CREATE_INSTANCE(&((*azh_handle)->h_tractOrPivotModelsHandle),10,
-    TractOrPivotModelDestroyInstance) != RETURNED)
+  TRACT_MODEL_HANDLE handle = *azh_handle;
+  m_ASSIGN_MAGIC_FIELD(TRACT_MODEL_HANDLE,handle)
+  m_TRACK_IF(GreenCollectionCreateInstance(&handle->h_tractOrPivotModelsHandle,
+    expectedTractOrPivotModelsNumber, sizeof(struct TRACT_OR_PIVOT_MODEL),
+      (GREEN_HANDLER__DISENGAGE_FUNCTION)NULL, (GREEN_HANDLER__COMPARE_FUNCTION)NULL,
+      (GREEN_HANDLER__EQUATE_FUNCTION)NULL, (void*)UNDEFINED) != RETURNED) 
   m_DIGGY_RETURN(RETURNED)
 } // TractModelCreateInstance
 
@@ -124,7 +127,7 @@ static int TractModelDestroyInstance (void *xhr_handle) {
 
   TRACT_MODEL_HANDLE xh_handle = (TRACT_MODEL_HANDLE) xhr_handle;
   m_CHECK_MAGIC_FIELD(TRACT_MODEL_HANDLE,xh_handle)
-  m_TRACK_IF(GStringsDestroyInstance(xh_handle->h_tractOrPivotModelsHandle) != RETURNED) 
+  m_TRACK_IF(GreenCollectionDestroyInstance(xh_handle->h_tractOrPivotModelsHandle) != RETURNED) 
 
   free(xh_handle);
 
@@ -135,17 +138,22 @@ static int TractModelDestroyInstance (void *xhr_handle) {
 
 struct PAMPHLET_MODEL {
   m_DECLARE_MAGIC_FIELD(PAMPHLET_MODEL_HANDLE)
-  G_STRINGS_HANDLE h_tractOrPivotModelsHandle ; // managed as NAMED OBJECTs  
+  struct P_STRING v_name; // that of as "named object"
+  GREEN_COLLECTION_HANDLE h_tractOrPivotModelsHandle;
 } ;
 typedef struct PAMPHLET_MODEL *PAMPHLET_MODEL_HANDLE;
 
 //
-static int PamphletModelCreateInstance(PAMPHLET_MODEL_HANDLE *azh_handle) {
+static int PamphletModelCreateInstance(PAMPHLET_MODEL_HANDLE *azh_handle,
+  int expectedTractOrPivotModelsNumber) {
   m_DIGGY_BOLLARD_S()
   m_MALLOC_INSTANCE(*azh_handle)
-  m_ASSIGN_MAGIC_FIELD(PAMPHLET_MODEL_HANDLE,*azh_handle)
-  m_TRACK_IF(NAMED_OBJECTS_CREATE_INSTANCE(&((*azh_handle)->h_tractOrPivotModelsHandle),10,
-    TractOrPivotModelDestroyInstance) != RETURNED)
+  PAMPHLET_MODEL_HANDLE handle = *azh_handle;
+  m_ASSIGN_MAGIC_FIELD(PAMPHLET_MODEL_HANDLE,handle)
+  m_TRACK_IF(GreenCollectionCreateInstance(&handle->h_tractOrPivotModelsHandle,
+    expectedTractOrPivotModelsNumber, sizeof(struct TRACT_OR_PIVOT_MODEL),
+      (GREEN_HANDLER__DISENGAGE_FUNCTION)NULL, (GREEN_HANDLER__COMPARE_FUNCTION)NULL,
+      (GREEN_HANDLER__EQUATE_FUNCTION)NULL, (void*)UNDEFINED) != RETURNED) 
   m_DIGGY_RETURN(RETURNED)
 } // PamphletModelCreateInstance
 
@@ -155,7 +163,7 @@ static int PamphletModelDestroyInstance (void *xhr_handle) {
 
   PAMPHLET_MODEL_HANDLE xh_handle = (PAMPHLET_MODEL_HANDLE) xhr_handle;
   m_CHECK_MAGIC_FIELD(PAMPHLET_MODEL_HANDLE,xh_handle)
-  m_TRACK_IF(GStringsDestroyInstance(xh_handle->h_tractOrPivotModelsHandle) != RETURNED) 
+  m_TRACK_IF(GreenCollectionDestroyInstance(xh_handle->h_tractOrPivotModelsHandle) != RETURNED) 
 
   free(xh_handle);
 
@@ -191,34 +199,31 @@ int EngelsCreateInstance(ENGELS_HANDLE *azh_handle) {
 // === II. examplars: === 
 // ======================
 
-// === TRACTs =====
-
-// ULTRA GREEN ITEM:
-struct TRACT_OCCURENCE {
-  // Localization: 
-  int offset ; // >= 0 (vis-a-vis tract) 
-} ;
-typedef struct TRACT_OCCURENCE *TRACT_OCCURENCE_STUFF;
+// === TRACT_OR_PIVOT =====
 
 // ULTRA GREEN ITEM:
 struct TRACT_OR_PIVOT {
-  char vb_pivot ;
+  int occurence; // 0 for 1st occurence (in tract)... 
+  int occurencesNumber; // (in tract) 
+  // Localization  
+  int offset ; // >= 0 (in tract) 
+  int length ; // >= 0 
+  // 
+  char vb_pivot ; // following tract or pivot model...
   union {
     int c_pivotModelEntry;
-    int c_tractModelEntry;
+    struct { 
+      int totalChildrenNumber; // total number of items to skip for next tract of pivot
+      int tractEntry; 
+    } c_tract;
   } select ;
 } ;
 typedef struct TRACT_OR_PIVOT *TRACT_OR_PIVOT_STUFF;
 
+// === TRACTs =====
 // See GREEN_ITEM @ c-ansi/green.h
 struct TRACT {
   int tractModelEntry;
-  // Localization 
-  int offset ; // >= 0 (vis-a-vis parent tract) 
-  int depth ; // >= 0 
-  int totalLength ; // >= 0 (all occurences)
-
-  GREEN_COLLECTION_HANDLE nh_tractOccurencesHandle; 
   GREEN_COLLECTION_HANDLE nh_tractOrPivotsHandle ; 
 } ;
 typedef struct TRACT *TRACT_STUFF;
@@ -229,20 +234,12 @@ typedef struct TRACT *TRACT_STUFF;
 // Ret:
 // - RETURNED: Ok
 // - -1: unexpected problem
-static int TractInitialize(TRACT_STUFF stuff,
-   int fieldsNumber) {
+static int TractInitialize(TRACT_STUFF stuff, int expectedItemsNumber) {
   m_DIGGY_BOLLARD_S()
-
-  if (stuff->nh_tractOccurencesHandle == NULL) {
-    m_TRACK_IF(GreenCollectionCreateInstance(&stuff->nh_tractOccurencesHandle,
-      BATEAU__EXPECTED_ITEMS_NUMBER, sizeof(struct TRACT_OCCURENCE),
-      (GREEN_HANDLER__DISENGAGE_FUNCTION)NULL, (GREEN_HANDLER__COMPARE_FUNCTION)NULL,
-      (GREEN_HANDLER__EQUATE_FUNCTION)NULL, (void*)UNDEFINED) != RETURNED) 
-  } // if
 
   if (stuff->nh_tractOrPivotsHandle == NULL) {
     m_TRACK_IF(GreenCollectionCreateInstance(&stuff->nh_tractOrPivotsHandle,
-      BATEAU__EXPECTED_ITEMS_NUMBER, sizeof(struct TRACT_OR_PIVOT),
+      expectedItemsNumber, sizeof(struct TRACT_OR_PIVOT),
       (GREEN_HANDLER__DISENGAGE_FUNCTION)NULL, (GREEN_HANDLER__COMPARE_FUNCTION)NULL,
       (GREEN_HANDLER__EQUATE_FUNCTION)NULL, (void*)UNDEFINED) != RETURNED) 
   } // if
@@ -251,21 +248,17 @@ static int TractInitialize(TRACT_STUFF stuff,
 } // TractInitialize
 
 // #SEE GREEN_HANDLER__DISENGAGE_FUNCTION @ c-ansi/green.h 
-static int PamphletTractDisengage(void *r_handle, char *r_greenItemStuff) {
+static int DisengageTract(void *r_handle, char *r_greenItemStuff) {
   m_DIGGY_BOLLARD_S()
 
   TRACT_STUFF tractStuff = (TRACT_STUFF) r_greenItemStuff;
-  if (tractStuff->nh_tractOccurencesHandle != NULL) {
-    m_TRACK_IF(GreenCollectionDestroyInstance(tractStuff->nh_tractOccurencesHandle) != RETURNED)
-    tractStuff->nh_tractOccurencesHandle = NULL;
-  }
   if (tractStuff->nh_tractOrPivotsHandle != NULL) {
     m_TRACK_IF(GreenCollectionDestroyInstance(tractStuff->nh_tractOrPivotsHandle) != RETURNED)
     tractStuff->nh_tractOrPivotsHandle = NULL;
   }
   
   m_DIGGY_RETURN(RETURNED)
-} // PamphletTractDisengage
+} // DisengageTract
 
 
 // ======== PAMPHLETs ======
@@ -273,14 +266,13 @@ static int PamphletTractDisengage(void *r_handle, char *r_greenItemStuff) {
 struct PAMPHLET {
   m_DECLARE_MAGIC_FIELD(PAMPHLET_HANDLE)
   ENGELS_HANDLE engelsHandle;
-  int pamphletModelEntry ;
-  struct P_STRING examplar ;
-  GREEN_COLLECTION_HANDLE h_tractsHandle ; 
+  int n_pamphletModelEntry ; // -1 special value => pamphlet is NOT defined 
+  struct P_STRING ci_examplar ; // Only significant if pamphlet is defined
+  GREEN_COLLECTION_HANDLE hcv_tractOrPivotsHandle ; // Only significant if pamphlet is defined ; 
+    // tracts or pivots then correspond to the "examplar" 
 } ; 
 
-// Ret:
-// - RETURNED: Ok
-// - -1 special value: anomaly raised
+// Public function; see .h
 int PamphletCreateInstance(PAMPHLET_HANDLE *azh_handle, ENGELS_HANDLE f_engelsHandle) {
   m_DIGGY_BOLLARD_S()
 
@@ -289,9 +281,10 @@ int PamphletCreateInstance(PAMPHLET_HANDLE *azh_handle, ENGELS_HANDLE f_engelsHa
   m_ASSIGN_MAGIC_FIELD(PAMPHLET_HANDLE,handle)
 
   handle->engelsHandle = f_engelsHandle;
-  m_TRACK_IF(GreenCollectionCreateInstance(&handle->h_tractsHandle, BATEAU__EXPECTED_ITEMS_NUMBER,
-    sizeof(struct TRACT), PamphletTractDisengage, (GREEN_HANDLER__COMPARE_FUNCTION)NULL,
-    (GREEN_HANDLER__EQUATE_FUNCTION)NULL, handle) != RETURNED)
+  m_TRACK_IF(GreenCollectionCreateInstance(&handle->hcv_tractOrPivotsHandle,
+    BATEAU__EXPECTED_ITEMS_NUMBER, sizeof(struct TRACT_OR_PIVOT),
+    (GREEN_HANDLER__DISENGAGE_FUNCTION)NULL, (GREEN_HANDLER__COMPARE_FUNCTION)NULL,
+    (GREEN_HANDLER__EQUATE_FUNCTION)NULL,(void*)UNDEFINED) != RETURNED)
 
   m_DIGGY_RETURN(RETURNED)
 } // PamphletCreateInstance
@@ -304,7 +297,7 @@ int PamphletDestroyInstance(void *xhr_handle) {
 
   PAMPHLET_HANDLE xh_handle = (PAMPHLET_HANDLE) xhr_handle;
   m_CHECK_MAGIC_FIELD(PAMPHLET_HANDLE,xh_handle)
-  m_TRACK_IF(GreenCollectionDestroyInstance(xh_handle->h_tractsHandle) != RETURNED)
+  m_TRACK_IF(GreenCollectionDestroyInstance(xh_handle->hcv_tractOrPivotsHandle) != RETURNED)
 
   free(xh_handle);
 
